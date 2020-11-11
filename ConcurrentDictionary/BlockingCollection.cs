902e681c1214c91e84e43cc5b0c2c7c12d3df76a
Farhan Kaioum <km1472233@gmail.com>
2020-11-11 11:54:55 +0600

    BlockingCollection added

---
+++ ConcurrentDictionary/BlockingCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentCollection
+{
+    public class BlockingCollection
+    {
+        // BlockingCollection uses for if you want to control your collection, like want to add into your collection at a time
+        // not more than 10 values, you can do it using BlockingCollection with /* bounded */ capacity.
+        // BlockingCollection uses produces consumer pattern, BlockingCollection not itself a collection, it's a wrapper collection
+        // Uderlying other collection
+        static BlockingCollection<int> messages = new BlockingCollection<int>(
+                            new ConcurrentBag<int>(), 10 /* bounded */
+        );
+
+        public static CancellationTokenSource cts = new CancellationTokenSource();
+
+        public static void ProduceAndConsume()
+        {
+            var producer = Task.Factory.StartNew(RunProducer);
+            var consumer = Task.Factory.StartNew(RunConsumer);
+
+            try
+            {
+                Task.WaitAll(new[] { producer, consumer }, cts.Token);
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e => true);
+            }
+        }
+
+        private static Random random = new Random();
+
+        private static void RunConsumer()
+        {
+            foreach (var item in messages.GetConsumingEnumerable()) // GetConsumingEnumerable special method, when it's call, it check if
+            {                                                       // the Enumerable/Collection is not empty, then it work
+                cts.Token.ThrowIfCancellationRequested();
+                Console.WriteLine($"-{item}");
+                Thread.Sleep(random.Next(1000));
+            }
+        }
+
+        private static void RunProducer()
+        {
+            while (true)
+            {
+                cts.Token.ThrowIfCancellationRequested();
+                int i = random.Next(100);
+                messages.Add(i);
+                Console.WriteLine($"+{i}\t");
+                Thread.Sleep(random.Next(1000));
+            }
+        }
+    }
+}
--- ConcurrentDictionary/Program.cs
+++ ConcurrentDictionary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ConcurrentCollection;
+using System;
+using System.Threading.Tasks;
 
 namespace ConcurrentCollections
 {
@@ -6,10 +8,15 @@ namespace ConcurrentCollections
     {
         static void Main(string[] args)
         {
-            ConcurrentDictionary.DictionaryP();
-            ConcurrentQueue.QueueP();
-            ConcurrentStack.StackP();
-            ConcurrentBag.BagP();
+            //ConcurrentDictionary.DictionaryP();
+            //ConcurrentQueue.QueueP();
+            //ConcurrentStack.StackP();
+            //ConcurrentBag.BagP();
+
+            // blocking collection
+            Task.Factory.StartNew(BlockingCollection.ProduceAndConsume, BlockingCollection.cts.Token);
+            Console.ReadKey();
+            BlockingCollection.cts.Cancel();
         }
     }
 }
