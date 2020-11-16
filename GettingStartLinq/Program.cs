c2e422ca71d1a4bc85bc0997f7ffa9b6f48c5c1d
Farhan Kaioum <km1472233@gmail.com>
2020-11-16 10:53:23 +0600

    Mastering Linq project added, How to implement IEnumerable example added

---
+++ GettingStartLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GettingStartLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImpelmentingIEnumerable.Print();
+        }
+    }
+}
