c938413657b6e2ca91e2e360bc2f3817f376144a
Farhan Kaioum <km1472233@gmail.com>
2020-11-20 15:01:06 +0600

    Lazy loading added

--- EFCORE-5/Controllers/HomeController.cs
+++ EFCORE-5/Controllers/HomeController.cs
@@ -6,20 +6,30 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WizLib.Models.Models;
+using WizLib_DataAccess.Data;
 
 namespace EFCORE_5.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var books = _context.Books.ToList();
+
+            foreach (var book in books)
+            {
+                _context.Entry(book).Reference(u => u.BookDetails).Load(); // when navigation property is single obj
+                //_context.Entry(book).Collection(u => u.Author).Load(); // if navigation property is collection
+            }
             return View();
         }
 
